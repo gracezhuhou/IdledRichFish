1e8abe58ac054008c38b68bd3797db1adf974e1e
wkw-karen <1196055210@qq.com>
2019-12-31 02:06:52 +0800

    category页面

--- app/src/main/java/com/sufe/idledrichfish/ui/publish/CategoryActivity.java
+++ app/src/main/java/com/sufe/idledrichfish/ui/publish/CategoryActivity.java
@@ -1,4 +1,15 @@
 package com.sufe.idledrichfish.ui.publish;
 
-public class CategoryActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sufe.idledrichfish.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
 }
